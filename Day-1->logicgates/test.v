bbbf625ee90c59cadd149fa0a3cd2c81671df1d5
SpoorthiVenkateshDiwakar <spoorthivdiwakar@gmail.com>
2024-10-16 18:59:58 +0530

    Update test.v

--- Day-1->logicgates/test.v
+++ Day-1->logicgates/test.v
@@ -1,1 +1,15 @@
+module test_bench(y,a,b);
+  output reg a,b;
+  intput wire y;
 
+  and_gate inst(.a(a),.b(b),.y(y));
+  initial begin
+    $monitor("Time=%0t a=%0b b=%0b y=%0b",$time,a,b,y);
+    #10 a=0; b=0;
+    #10 a=0; b=1;
+    #10 a=1; b=0;
+    #10 a=1; b=1;
+    #10
+    $finish
+  end
+endmodule
