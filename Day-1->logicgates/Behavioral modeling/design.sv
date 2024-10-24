2e4fe733766632c925b93de079f04797130bdba5
SpoorthiVenkateshDiwakar <spoorthivdiwakar@gmail.com>
2024-10-24 10:11:21 +0530

    Create design.sv

---
+++ Day-1->logicgates/Behavioral modeling/design.sv
@@ -0,0 +1,14 @@
+module logic_gates(
+  input wire a,b;
+  output reg and_gate,or_gate,not_gate,nand_gate,nor_gate,xor_gate,xnor_gate;
+);
+  always @(*) begin
+    and_gate = a&b;
+    or_gate = a|b;
+    not_gate = ~a;
+    nand_gate = ~(a&b);
+    nor_gate = ~(a|b);
+    xor_gate = a^b;
+    xnor_gate = ~(a^b);
+  end
+endmodule
